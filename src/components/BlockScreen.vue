e1ef3cdfd5834714697bb1c7633cbc76e33eaeec
mccrush <mccrush2027@gmail.com>
2025-03-04 10:39:24 +0700

    Settingw view heights

--- src/components/BlockScreen.vue
+++ src/components/BlockScreen.vue
@@ -1,6 +1,15 @@
 <template>
   <div
-    class="d-flex flex-column justify-content-center align-items-center bg-body-secondary vh-100"
+    class="my-vh-50 d-flex d-md-none flex-column justify-content-center align-items-center bg-body-secondary"
+  >
+    <img
+      class="my-screen-image"
+      src="/images/cover/bg-cover-border.png"
+      alt="MWP"
+    />
+  </div>
+  <div
+    class="d-none d-md-flex flex-column justify-content-center align-items-center bg-body-secondary vh-100"
   >
     <img
       class="my-screen-image"
@@ -14,5 +23,9 @@
 .my-screen-image {
   width: 100%;
   max-width: 1040px;
+}
+
+.my-vh-50 {
+  height: 50dvh;
 }
 </style>
--- src/components/BlockVideo.vue
+++ src/components/BlockVideo.vue
@@ -1,6 +1,19 @@
 <template>
   <div
-    class="d-flex flex-column justify-content-center align-items-center bg-dark vh-100"
+    class="my-vh-50 d-flex d-md-none flex-column justify-content-center align-items-center bg-dark"
+  >
+    <iframe
+      class="my-video"
+      src="https://rutube.ru/play/embed/270a800480c4071b9543f01ef123f045/"
+      frameBorder="0"
+      allow="clipboard-write; autoplay"
+      webkitAllowFullScreen
+      mozallowfullscreen
+      allowFullScreen
+    ></iframe>
+  </div>
+  <div
+    class="d-none d-md-flex flex-column justify-content-center align-items-center bg-dark vh-100"
   >
     <iframe
       class="my-video"
@@ -18,12 +31,10 @@
 .my-video {
   width: 100%;
   max-width: 990px;
-  height: calc(100% / 1.8);
+  height: calc(100% / 1.7);
 }
 
-/* 
-width="720"
-      height="405"
-
-*/
+.my-vh-50 {
+  height: 50dvh;
+}
 </style>
